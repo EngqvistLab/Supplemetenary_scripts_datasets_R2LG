2d7fccc990705f127dca29d4d7c93c0c7ed0283e
Gang Li <gangl@vera2.int.private>
2019-12-10 14:05:59 +0100

    add script for classical regression models

---
+++ EvaluateClassicalModels/README.md
@@ -0,0 +1,26 @@
+#### Usage:
+
+```
+python run_model_evaluation.py infile.csv outdir
+```
+
+`infile.csv` is an input comma-sperated file with first column as index and last column is the target column. Other columns are the features. The script will firstly standardize each column and then test the performance of six different regression models via a nested cross validation approach. Those six models are:
+```python
+sklearn.linear_model.LinearRegression
+sklearn.linear_model.BayesianRidge
+sklearn.linear_model.ElasticNetCV
+sklearn.tree.DecisionTreeRegressor
+sklearn.svm.SVR
+sklearn.ensemble.RandomForestRegressor
+```
+
+`outdir` is the directory where output file will be created. The name of the outputfile is `infile.out`. It is a csv file contains the mean and standard deviation of R2 score in the cross validation, for example:
+```
+model,mean,std
+Linear model,0.5606470243036312,0.05182762180825625
+Elastic Net,0.5561527172818607,0.046980868519725986
+BayesRige,0.5613494374587507,0.051039246940720234
+SVR model,0.547889662217659,0.026893484452659596
+Tree model,0.5355491267446859,0.03453184980344376
+Random forest,0.6021454315518417,0.028483260693779797
+```
---
+++ EvaluateClassicalModels/run_model_evaluation.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### 1. try different regression models
+# ##### Gang Li, 2018-09-21
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression as LR
+from sklearn import svm
+from sklearn.linear_model import ElasticNetCV
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import BayesianRidge
+import sys
+from multiprocessing import cpu_count
+
+import os
+
+def normalize(X):
+    X_n = np.zeros_like(X)
+    k = 0
+    for i in range(X.shape[1]):
+        x = X[:,i]
+        if np.var(x) == 0: continue
+        X_n[:,k] = (x-np.mean(x))/np.var(x)**0.5
+        k+=1
+    return np.around(X_n, decimals=3)
+
+
+
+
+def do_cross_validation(X,y,model):
+    scores = cross_val_score(model,X,y,scoring='r2',cv=5,n_jobs=20)
+    res = str(np.mean(scores))+','+str(np.std(scores))+'\n'
+    print(res)
+    print(scores)
+    return res
+
+
+
+
+def lr():
+    return LR()
+
+
+
+
+def elastic_net():
+    return ElasticNetCV(n_jobs=20)
+
+
+
+def bayesridge():
+    model = BayesianRidge()
+    return model
+
+
+
+
+def svr():
+    parameters={
+                'C':np.logspace(-5,10,num=16,base=2.0),
+                'epsilon':[0,0.01,0.1,0.5,1.0,2.0,4.0]
+                }
+    svr = svm.SVR(kernel='rbf')
+    model = GridSearchCV(svr,parameters,n_jobs=cpu_count(),cv=3)
+    return model
+
+
+
+def tree():
+    parameters={
+                'min_samples_leaf':np.linspace(0.01,0.5,10)
+                }
+    dtr=DecisionTreeRegressor()
+    model=GridSearchCV(dtr,parameters,n_jobs=cpu_count(),cv=3)
+    return model
+
+
+
+def random_forest():
+    parameters = {
+                    'max_features':np.arange(0.1,1.1,0.1)
+    }
+    rf = RandomForestRegressor(n_estimators=1000,verbose =1,n_jobs=-1)
+    model=GridSearchCV(rf,parameters,n_jobs=1,cv=3)
+    return model
+
+
+def test_model_performace(infile,outfile):
+    df = pd.read_csv(infile,index_col=0)
+    print(df.shape)
+    df = df.round(3)
+    X,y = df.values[:,:-1],df.values[:,-1]
+    print(X.shape)
+    X = normalize(X)
+    print(X.shape)
+   
+    fhand = open(outfile,'w')
+    fhand.write('model,mean,std\n')
+    
+    fhand.write('Linear model,')
+    fhand.write(do_cross_validation(X,y,lr()))
+    
+    fhand.write('Elastic Net,')
+    fhand.write(do_cross_validation(X,y,elastic_net()))
+    
+    fhand.write('BayesRige,')
+    fhand.write(do_cross_validation(X,y,bayesridge()))
+    
+    fhand.write('SVR model,')
+    fhand.write(do_cross_validation(X,y,svr()))
+    
+    fhand.write('Tree model,')
+    fhand.write(do_cross_validation(X,y,tree()))
+
+    fhand.write('Random forest,')
+    fhand.write(do_cross_validation(X,y,random_forest()))
+    
+    fhand.close()
+
+
+if __name__ == "__main__":
+    infile = sys.argv[-2]
+    outdir = sys.argv[-1]
+    outfile = os.path.join(outdir,infile.split('/')[-1].replace('.csv','.out'))
+    test_model_performace(infile,outfile)
+
